ed26985876668cadd4fab99524865c561c5f970c
nurafifah <naafifah98@gmail.com>
2020-04-15 18:00:26 +0800

    Operator Binary Assigmnet

---
+++ src/Operator/operatorAssigment.java
@@ -0,0 +1,11 @@
+package Operator;
+
+public class operatorAssigment {
+    //opertaor (=) Sebagai Assigment / penuugasan
+    //menempatkan nilai kedalam variable
+    //baris 5 -7 adalah comment / komentar yang tidak dijakankan oleh program
+
+    String nama ="Nur Afifah";
+    int jumlahSks = 24;
+    double indekPrestasiKumulatif =3.89;
+}
