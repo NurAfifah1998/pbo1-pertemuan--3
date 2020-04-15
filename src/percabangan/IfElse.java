4aa740a979083cac2c5b65f209b4bb9c099764b7
nurafifah <naafifah98@gmail.com>
2020-04-15 19:14:19 +0800

    Stetmen 1terbaca condition true

---
+++ src/percabangan/IfElse.java
@@ -0,0 +1,18 @@
+package percabangan;
+
+public class IfElse {
+    public static void main(String[] args) {
+        double totalBelanja = 53000;
+        double uangDidompet = 25000;
+
+        if (uangDidompet<totalBelanja){
+            System.out.println("uang kurang , silahkan belanja sesuai uang anda");
+        }else{
+            System.out.println("uang pass coyy ...");
+        }
+
+
+    }
+
+
+}
