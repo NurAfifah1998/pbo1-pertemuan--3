2464b8986398c1702d5733a665b2f2121da65702
nurafifah <naafifah98@gmail.com>
2020-04-15 18:01:50 +0800

    Operator Binary Aritmatika

--- src/Operator/operatorAssigment.java
+++ src/Operator/operatorAssigment.java
@@ -8,4 +8,15 @@ public class operatorAssigment {
     String nama ="Nur Afifah";
     int jumlahSks = 24;
     double indekPrestasiKumulatif =3.89;
+
+    public static void  main(String[] args){
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+
+
+    }
 }
